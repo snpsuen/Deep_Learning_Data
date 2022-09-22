09012640a03b14c1f40c25466c01ea8cdc4a0532
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-09-22 18:39:44 +0800

    Create try_fashion_example.py

---
+++ script/try_fashion_example.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+import cv2
+from tensorflow import keras
+from numpy import asarray, argmax
+
+img = cv2.imread('./fashion_example.jpg', cv2.IMREAD_GRAYSCALE)
+print('Before preprossing, img.shape = %s' %(img.shape))
+
+imgsize = 28
+img = tf.expand_dims(img, -1)
+img = tf.divide(img, 255)
+img = tf.image.resize(img, [imgsize, imgsize])
+img = tf.reshape(img, [1, imgsize, imgsize, 1])
+print('After preprossing, img.shape = %s' %(img.shape))
