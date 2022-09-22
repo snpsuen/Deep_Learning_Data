3b6b165203a2af7a7359bc739f3f6dacdf14374a
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-09-22 21:21:46 +0800

    Update try_fashion_example.py

--- script/try_fashion_example.py
+++ script/try_fashion_example.py
@@ -4,11 +4,11 @@ from tensorflow import keras
 from numpy import asarray, argmax
 
 img = cv2.imread('./fashion_example.jpg', cv2.IMREAD_GRAYSCALE)
-print('Before preprossing, img.shape = %s' %(img.shape))
+print("Before preprossing, img.shape = " img.shape)
 
 imgsize = 28
 img = tf.expand_dims(img, -1)
 img = tf.divide(img, 255)
 img = tf.image.resize(img, [imgsize, imgsize])
 img = tf.reshape(img, [1, imgsize, imgsize, 1])
-print('After preprossing, img.shape = %s' %(img.shape))
+print("After preprossing, img.shape = " img.shape)
