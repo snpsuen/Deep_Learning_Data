84ac2faa72d5204bef43b0431bac8d3510b08474
snpsuen <66404197+snpsuen@users.noreply.github.com>
2022-09-22 17:44:09 +0800

    Update use_cnn_fashion_model.py

--- script/use_cnn_fashion_model.py
+++ script/use_cnn_fashion_model.py
@@ -5,14 +5,14 @@ from numpy import asarray, argmax
 
 model = tf.keras.models.load_model("./cnn_fashion_class_model")
 img = cv2.imread('./1042.png', cv2.IMREAD_GRAYSCALE)
-print("img.shape = %s" % (img.shape))
+print("Before preprossing, img.shape = " % (img.shape))
 
 imgsize = 28
 img = tf.expand_dims(img, -1)
 img = tf.divide(img, 255)
 img = tf.image.resize(img, [imgsize, imgsize])
-img = tf.reshape(img, [1, img_size, img_size, 1])
-print("After preprossing, img.shape = %s" % (img.shape))
+img = tf.reshape(img, [1, imgsize, imgsize, 1])
+print("After preprossing, img.shape = " % (img.shape))
 
 classnames = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 yhat = model.predict(img)
